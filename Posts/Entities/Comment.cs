10013d74b435748e12f28933e8ab78027f6a2513
diegolins10 <diegofernandeslins@gmail.com>
2021-07-12 22:19:34 -0300

    Comment class

---
+++ Posts/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Posts.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
