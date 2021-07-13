13c83070dc9dbd8aaf5b4c2180afe8a5685824b5
diegolins10 <diegofernandeslins@gmail.com>
2021-07-12 22:19:42 -0300

    Post class

---
+++ Posts/Entities/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Posts.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> ListComments { get; set; } = new List<Comment>(); // associando post com
+                                                                               // varios coments com uma list 
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            ListComments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            ListComments.Remove(comment);
+        }
+    }
+}
